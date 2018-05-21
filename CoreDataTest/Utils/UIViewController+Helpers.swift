5100412ec6701bd0bcdac5aeee3f18161869f37e
Dylan Bruschi <dylanbruschi54@gmail.com>
2018-05-21 11:33:58 -0500

    Subject line (try to keep under 50 characters) Create EmployeeController and CreateEmployeeController Multi-line description of commit Use imperative voice Wrap text at 72 characters
    Create EmployeeController and CreateEmployeeController
    Refactor code to create cancel and plus buttons into UIVC extension
    
    [Ticket: X]

---
+++ CoreDataTest/Utils/UIViewController+Helpers.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Helpers.swift
+//  CoreDataTest
+//
+//  Created by Dylan Bruschi on 5/21/18.
+//  Copyright © 2018 Dylan Bruschi. All rights reserved.
+//
+
+import Foundation
