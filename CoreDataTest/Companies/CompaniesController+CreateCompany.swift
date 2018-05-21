a08aec9e7930811c7bcf1f144790462e0d245990
Dylan Bruschi <dylanbruschi54@gmail.com>
2018-05-21 09:48:43 -0500

    Subject line (try to keep under 50 characters) Refactor CompaniesController with protocols Multi-line description of commit Use imperative voice Wrap text at 72 characters
    Move code in CompaniesController to separate extension files
    with CreateCompanyVC and UITableView delegate methods respectively
    
    [Ticket: X]

---
+++ CoreDataTest/Companies/CompaniesController+CreateCompany.swift
@@ -0,0 +1,9 @@
+//
+//  CompaniesController+CreateCompany.swift
+//  CoreDataTest
+//
+//  Created by Dylan Bruschi on 5/20/18.
+//  Copyright © 2018 Dylan Bruschi. All rights reserved.
+//
+
+import Foundation
---
+++ CoreDataTest/Companies/CompaniesController+UITableView.swift
@@ -0,0 +1,9 @@
+//
+//  CompaniesController+UITableView.swift
+//  CoreDataTest
+//
+//  Created by Dylan Bruschi on 5/20/18.
+//  Copyright © 2018 Dylan Bruschi. All rights reserved.
+//
+
+import Foundation
