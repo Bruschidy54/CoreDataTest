c51e02ca1991437c0629637b36e9bd9e63aa7b1b
Dylan Bruschi <dylanbruschi54@gmail.com>
2018-05-22 16:43:16 -0500

    Organize Employees Into EmployeeTypes
    Add EmployeeType enum
    Place EmployeeType segmented control into CreateEmployeeVC
    Organize sections of tableView in EmployeeController by EmployeeType
    
    [Ticket: X]

---
+++ CoreDataTest/Companies/Employees/EmployeeType.swift
@@ -0,0 +1,15 @@
+//
+//  EmployeeType.swift
+//  CoreDataTest
+//
+//  Created by Dylan Bruschi on 5/22/18.
+//  Copyright © 2018 Dylan Bruschi. All rights reserved.
+//
+
+import Foundation
+
+enum EmployeeType: String {
+    case Executive
+    case SeniorManagment = "Sr Management"
+    case Staff
+}
---
+++ CoreDataTest/Utils/IndentedLabel.swift
@@ -0,0 +1,9 @@
+//
+//  IndentedLabel.swift
+//  CoreDataTest
+//
+//  Created by Dylan Bruschi on 5/22/18.
+//  Copyright © 2018 Dylan Bruschi. All rights reserved.
+//
+
+import Foundation
